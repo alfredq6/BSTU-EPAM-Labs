0b5ab8573e37259a8dfedb990cee0a126bdb6ac0
Maxim Kaloupaiev <alfredq6@gmail.com>
2019-09-26 09:26:08 +0300

    add triangle inequality project and tests

---
+++ TriangleProj/Triangle.Tests/TriangleSidesPossbilityTest.cs
@@ -0,0 +1,58 @@
+using NUnit.Framework;
+using TriangleProj;
+
+namespace Tests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [TestCase(0)]
+        [TestCase(-5)]
+        public void SideACannotGetZeroAndLess(int value)
+        {
+            Assert.IsFalse(Triangle.CheckIsPossibleToCreate(value, 1, 2));
+        }
+
+        [TestCase(0)]
+        [TestCase(-5)]
+        public void SideBCannotGetZeroAndLess(int value)
+        {
+            Assert.IsFalse(Triangle.CheckIsPossibleToCreate(1, value, 2));
+        }
+
+        [TestCase(0)]
+        [TestCase(-5)]
+        public void SideCCannotGetZeroAndLess(int value)
+        {
+            Assert.IsFalse(Triangle.CheckIsPossibleToCreate(3, 1, value));
+        }
+
+        [Test]
+        public void SideANotLessBAndCSum()
+        {
+            Assert.IsFalse(Triangle.CheckIsPossibleToCreate(4, 1, 3));
+        }
+
+        [Test]
+        public void SideBNotLessCAndASum()
+        {
+            Assert.IsFalse(Triangle.CheckIsPossibleToCreate(1, 4, 3));
+        }
+
+        [Test]
+        public void SideCNotLessBAndASum()
+        {
+            Assert.IsFalse(Triangle.CheckIsPossibleToCreate(3, 4, 1));
+        }
+
+        [Test]
+        public void TrueSides()
+        {
+            Assert.IsTrue(Triangle.CheckIsPossibleToCreate(1, 1, 1));
+        }
+    }
+}
---
+++ TriangleProj/TriangleProj/Triangle.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TriangleProj
+{
+    public class Triangle
+    {
+        public double A { get; private set; }
+        public double B { get; private set; }
+        public double C { get; private set; }
+
+        private Triangle() { }
+
+        public static bool CheckIsPossibleToCreate(double a, double b, double c)
+        {
+            if (a > 0 && b > 0 && c > 0 && a + b > c && b + c > a && a + c > b)
+                return true;
+            return false;
+        }
+
+        public Triangle GetInstance(double a, double b, double c)
+        {
+            if (CheckIsPossibleToCreate(a, b, c))
+                return new Triangle() { A = a, B = b, C = c };
+            return null;
+        }
+    }
+}
